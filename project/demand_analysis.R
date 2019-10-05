d221989d88a18a0c2945a2405c8ae1a43a0e53d4
nesaking100 <josh@looker.co.nz>
2019-10-05 21:59:57 +1300

    finalised demand forecasts, added ANOVA for justification

---
+++ project/demand_analysis.R
@@ -0,0 +1,89 @@
+#Setting current working directory to source file location
+rstudioapi::getActiveDocumentContext
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#Loading required libraries
+library(tidyverse)
+library(ggplot2)
+library(s20x)
+
+#Reading in data and cleaning
+demand = read_csv("demandData.csv", col_names=TRUE)
+demand = gather(demand,key=date,value=demand,ends_with("19"))
+demand$weekday = as.factor(weekdays(as.Date(demand$date,format="%d/%m/%Y")))
+demand$Type <- sub("(\\w+).*", "\\1", demand$Supermarket)
+demand$Type = as.factor(demand$Type)
+demand$Supermarket = as.factor(demand$Supermarket)
+
+weekday <- filter(demand,weekday%in% c("Monday","Tuesday","Wednesday","Thursday","Friday"))
+weekend <- filter(demand,weekday=="Saturday")
+four.day <- filter(weekday,Type=="Four")
+fresh.day <- filter(weekday,Type=="Fresh")
+new.day <- filter(weekday,Type=="New")
+pak.day <- filter(weekday,Type=="Pak")
+four.end <- filter(weekend,Type=="Four")
+fresh.end <- filter(weekend,Type=="Fresh")
+new.end <- filter(weekend,Type=="New")
+pak.end <- filter(weekend,Type=="Pak")
+
+f.d.fit = lm(demand~Supermarket,data=four.day)
+n.d.fit = lm(demand~Supermarket,data=new.day)
+p.d.fit = lm(demand~Supermarket,data=pak.day)
+
+f.e.fit = lm(demand~Supermarket,data=four.end)
+n.e.fit = lm(demand~Supermarket,data=new.end)
+p.e.fit = lm(demand~Supermarket,data=pak.end)
+
+four.day.g <- group_by(four.day,Supermarket)
+four.day.d <- data.frame(Supermarket=four.day$Supermarket[1:11],demand=ceiling(mean(four.day$demand)))
+four.day.s <- summarise(four.day.g,sum=sum(demand))
+four.end.g <- group_by(four.end,Supermarket)
+four.end.d <- data.frame(Supermarket=four.end$Supermarket[1:11],demand=ceiling(mean(four.end$demand)))
+four.end.s <- summarise(four.end.g,sum=sum(demand))
+
+fresh.day.g <- group_by(fresh.day,Supermarket)
+fresh.day.d <- data.frame(Supermarket=fresh.day$Supermarket[1],demand=ceiling(mean(fresh.day$demand)))
+fresh.day.s <- summarise(fresh.day.g,sum=sum(demand))
+fresh.end.g <- group_by(fresh.end,Supermarket)
+fresh.end.d <- data.frame(Supermarket=fresh.end$Supermarket[1],demand=ceiling(mean(fresh.end$demand)))
+fresh.end.s <- summarise(fresh.end.g,sum=sum(demand))
+
+new.day.g <- group_by(new.day,Supermarket)
+new.day.d <- data.frame(Supermarket=new.day$Supermarket[1:19],demand=ceiling(mean(new.day$demand)))
+new.day.s <- summarise(new.day.g,sum=sum(demand))
+new.end.g <- group_by(new.end,Supermarket)
+new.end.d <- data.frame(Supermarket=new.end$Supermarket[1:19],demand=ceiling(mean(new.end$demand)))
+new.end.s <- summarise(new.end.g,sum=sum(demand))
+
+pak.day.g <- group_by(pak.day,Supermarket)
+pak.day.d <- data.frame(Supermarket=pak.day$Supermarket[1:15],demand=ceiling(mean(pak.day$demand)))
+pak.day.s <- summarise(pak.day.g,sum=sum(demand))
+pak.end.g <- group_by(pak.end,Supermarket)
+pak.end.d <- data.frame(Supermarket=pak.end$Supermarket[1:15],demand=ceiling(mean(pak.end$demand)))
+pak.end.s <- summarise(pak.end.g,sum=sum(demand))
+
+
+
+plan.day <- rbind(four.day.d,fresh.day.d,new.day.d,pak.day.d)
+error.day <- rbind(four.day.s,fresh.day.s,new.day.s,pak.day.s)
+error.day$demand <- plan.day$demand
+error.day$error <- error.day$demand*20/error.day$sum
+error.day$fraction <- abs(error.day$error-1)
+error.day$sum <- sum(error.day$fraction)
+
+plan.end <- rbind(four.end.d,fresh.end.d,new.end.d,pak.end.d)
+error.end <- rbind(four.end.s,fresh.end.s,new.end.s,pak.end.s)
+error.end$demand <- plan.end$demand
+error.end$error <- error.end$demand*4/error.end$sum
+error.end$fraction <- abs(error.end$error-1)
+error.end$sum <- sum(error.end$fraction)
+
+a.d <- rbind(anova(f.d.fit)[1,],anova(f.e.fit)[1,],anova(n.d.fit)[1,],anova(n.e.fit)[1,],anova(p.d.fit)[1,],anova(p.e.fit)[1,])
+anova.d <- data.frame(Brand = c("Four","Four","New","New","Pak","Pak"), Day = c("M-F","Sat","M-F","Sat","M-F","Sat"),P.value = a.d[,5])
+
+write.csv(plan.day,'weekdaydemand.csv',row.names = FALSE)
+write.csv(plan.end,'weekenddemand.csv',row.names = FALSE)
+write.csv(error.day,'weeklyerror.csv',row.names = FALSE)
+write.csv(error.end,'weekenderror.csv',row.names = FALSE)
+write.csv(anova.d,'anova.csv',row.names = FALSE)
+
