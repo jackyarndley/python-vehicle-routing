1bd026f568bc7fef8dc52bf7b40a4aca8243eeb8
jackyarndley <34801340+jackyarndley@users.noreply.github.com>
2019-10-10 14:38:17 +1300

    added files for weekend run

--- project/vrp_solver.py
+++ project/vrp_solver.py
@@ -14,7 +14,7 @@ from pulp import LpVariable, LpProblem, LpBinary, LpMinimize, lpSum, LpStatus, v
 # Load the specific data files into pandas dataframes
 data = pd.read_csv('data/new_durations.csv', index_col=0)
 data2 = pd.read_csv('data/new_locations.csv', index_col=1)
-data3 = pd.read_csv('data/weekdaydemand.csv', index_col=0)
+data3 = pd.read_csv('data/weekenddemand.csv', index_col=0)
 
 # OpenRouteService key - this is mine
 ORS_KEY = '5b3ce3597851110001cf62482926c2987d7f46118f341e666eb30010'
